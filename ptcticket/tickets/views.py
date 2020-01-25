3d6faada1434766b33be73f04bb747f72856e6a8
Robert Osonma <35873421+master248@users.noreply.github.com>
2020-01-25 17:50:16 -0600

    adding a new view

--- ptcticket/tickets/views.py
+++ ptcticket/tickets/views.py
@@ -5,3 +5,7 @@ from .models import Tickets
 # Create your views here.
 def index(request):
      return render(request, 'students/index.html')
+# def index(request):
+#     students = Tickets.objects.all()
+#     output = ', '.join([t.name for t in students])
+#     return HttpResponse(output)
