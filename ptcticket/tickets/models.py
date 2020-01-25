cf954ae7113501d6f2dbb4efebcdccf73e5a9c6e
Robert Osonma <35873421+master248@users.noreply.github.com>
2020-01-25 15:16:01 -0600

    working on the database

--- ptcticket/tickets/models.py
+++ ptcticket/tickets/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    name = models.CharField(max_length=15)
+    course = models.CharField(max_length=8)
+    question = models.CharField(max_length=200)
+    completed = models.BooleanField()
+    timestamp = models.DateTimeField(auto_now_add= True)
