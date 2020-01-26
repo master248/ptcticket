cd839f289169cf31b5a0133a39ece3505fc5ddca
Robert Osonma <35873421+master248@users.noreply.github.com>
2020-01-25 22:05:34 -0600

    working on html

--- ptcticket/tickets/views.py
+++ ptcticket/tickets/views.py
@@ -7,19 +7,19 @@ from .models import Tickets
 
 def tickets_detail(request):
 
-     if request.method == 'POST':
-          form = TicketsForm(request.POST)
-          if form.is_valid():
-               form.save()
-               return render(request, 'succ.html')
+    if request.method == 'POST':
+        form = TicketsForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'succ.html')
 
-     form = TicketsForm()
-     return render(request, 'students.html', {'form': form})
+    form = TicketsForm()
+    return render(request, 'students.html', {'form': form})
 def manage(request):
-    if request.method == 'POST'
+    if request.method == 'POST':
         form = ManageForm(request.POST)
         student_tickets = Tickets.objects.filter(completed=False)
-        if form.is_valid()
-            form.save()
-    form = ManageForm(request.POST)
+        if form.is_valid():
+            completed = Tickets.objects
+    form = ManageForm()
     return render(request, 'teachers.html', {'student_tickets': student_tickets,'form': form})
