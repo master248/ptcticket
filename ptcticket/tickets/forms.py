3356b25227537cb761b879d8ac0bd6544ba5039b
Robert Osonma <35873421+master248@users.noreply.github.com>
2020-01-25 20:01:23 -0600

    made form with students

--- ptcticket/tickets/forms.py
+++ ptcticket/tickets/forms.py
@@ -3,3 +3,6 @@ from django import forms
 class ContactForm(forms.Form):
     name = forms.CharField()
     class_category = forms.ChoiceField(choices=[('121', '121'), ('other', 'Other')])
+class ManageForm(forms.Form):
+    pt = forms.CharField()
+    
--- ptcticket/tickets/views.py
+++ ptcticket/tickets/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.http import HttpResponse
 from .forms import ContactForm
+from .forms import ManageForm
 from .models import Tickets
 
 def contact(request):
      form = ContactForm()
      return render(request, 'students.html', {'form': form})
 def manage(request):
+    form = ManageForm()
     student_tickets = Tickets.objects.filter(completed=False)
-    return render(request, 'teachers.html', {'student_tickets': student_tickets})
+    return render(request, 'teachers.html', {'student_tickets': student_tickets,'form': form})
