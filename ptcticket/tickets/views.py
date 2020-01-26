5d7eebec72e6188c044642032b131f51147767cd
Robert Osonma <35873421+master248@users.noreply.github.com>
2020-01-25 21:47:11 -0600

    working on teachers

--- ptcticket/tickets/views.py
+++ ptcticket/tickets/views.py
@@ -9,6 +9,10 @@ def contact(request):
      form = ContactForm()
      return render(request, 'students.html', {'form': form})
 def manage(request):
-    form = ManageForm()
-    student_tickets = Tickets.objects.filter(completed=False)
+    if request.method == 'POST'
+        form = ManageForm(request.POST)
+        student_tickets = Tickets.objects.filter(completed=False)
+        if form.is_valid()
+            form.save()
+    form = ManageForm(request.POST)
     return render(request, 'teachers.html', {'student_tickets': student_tickets,'form': form})
