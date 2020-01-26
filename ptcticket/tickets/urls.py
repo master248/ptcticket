6c47eb09a8766ced351f1b8dd558a79cd83b5be7
Robert Osonma <35873421+master248@users.noreply.github.com>
2020-01-25 19:49:06 -0600

    added teacher page

--- ptcticket/tickets/urls.py
+++ ptcticket/tickets/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.contact),
+    path('teachers/', views.manage),
 ]
--- ptcticket/tickets/views.py
+++ ptcticket/tickets/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.http import HttpResponse
 from .forms import ContactForm
+from .models import Tickets
 
 def contact(request):
      form = ContactForm()
      return render(request, 'students.html', {'form': form})
+def manage(request):
+    student_tickets = Tickets.objects.filter(completed=False)
+    return render(request, 'teachers.html', {'student_tickets': student_tickets})
