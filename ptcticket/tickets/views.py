0d725923cd264f85fa606845e88450d0a347cb7d
Robert Osonma <35873421+master248@users.noreply.github.com>
2020-01-25 18:49:56 -0600

    testing code

--- ptcticket/tickets/views.py
+++ ptcticket/tickets/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 from .models import Tickets
 
 # Create your views here.
 def index(request):
-     return render(request, 'students/index.html')
+    student_tickets = Tickets.objects.all()
+    template = loader.get_template('students/index.html')
+    #output = ', '.join([t.name for t in students])
+    context = {'student_tickets': student_tickets}
+    return render(request, 'students/index.html', context)
 # def index(request):
-#     students = Tickets.objects.all()
-#     output = ', '.join([t.name for t in students])
-#     return HttpResponse(output)
+#      return render(request, 'students/index.html')
+#return HttpResponse(output)
